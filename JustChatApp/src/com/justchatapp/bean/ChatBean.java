0d6659e3b714455c8c77496471c5e147a905c423
Rami <rami.alsawadi1@gmail.com>
2016-01-09 18:55:25 +0100

    Q.Q

--- JustChatApp/src/com/justchatapp/bean/ChatBean.java
+++ JustChatApp/src/com/justchatapp/bean/ChatBean.java
@@ -1,83 +1,83 @@
-package com.justchatapp.bean;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.wink.client.Resource;
-import org.apache.wink.client.RestClient;
-
-import com.google.gson.Gson;
-
-import com.justchatapp.vm.ChatViewModel;
-
-@ViewScoped
-@ManagedBean(name = "chatBean")
-public class ChatBean implements Serializable {
-	private String path = "http://130.237.84.211:8080/Faceoogle2/rest/";
-	private static final long serialVersionUID = 1L;
-	private List<ChatViewModel> chatMessages = new ArrayList<ChatViewModel>();
-	private String paramUser = ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
-			.getRequest()).getParameter("user");
-	private String message;
-	@ManagedProperty(value = "#{userBean}")
-	private UserBean userBean;
-
-	public void setUserBean(UserBean userBean) {
-		this.userBean = userBean;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<ChatViewModel> getChatMessages() {
-		RestClient client = new RestClient();
-		Resource res = client.resource(path + "chat/history?chater=" + userBean.getUsername() + "&chatee="+paramUser);
-		String jsonChat = res.accept("application/json").get(String.class);
-		Gson gson = new Gson();
-		chatMessages = gson.fromJson(jsonChat, ArrayList.class);
-		return chatMessages;
-	}
-
-	public void setChatMessages(List<ChatViewModel> chatMessages) {
-		this.chatMessages = chatMessages;
-	}
-
-	public String getParamUser() {
-		return paramUser;
-	}
-
-	public void setParamUser(String paramUser) {
-		this.paramUser = paramUser;
-	}
-	
-	public String sendMessage() {
-		if(paramUser == null) {
-			return "index.xhtml";
-		}
-		Map<String, String> msg = new HashMap<String, String>();
-		msg.put("chater", userBean.getUsername());
-		msg.put("chatee", paramUser);
-		msg.put("message", message);
-		Gson gson = new Gson();
-		String json = gson.toJson(msg);
-		RestClient client = new RestClient();
-		Resource resource = client.resource(path + "chat/sendmessage");
-		resource.contentType("application/json").accept("text/plain").post(String.class, json);
-		return null;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-}
+//package com.justchatapp.bean;
+//
+//import java.io.Serializable;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.ManagedProperty;
+//import javax.faces.bean.ViewScoped;
+//import javax.faces.context.FacesContext;
+//import javax.servlet.http.HttpServletRequest;
+//
+//import org.apache.wink.client.Resource;
+//import org.apache.wink.client.RestClient;
+//
+//import com.google.gson.Gson;
+//
+//import com.justchatapp.vm.ChatViewModel;
+//
+//@ViewScoped
+//@ManagedBean(name = "chatBean")
+//public class ChatBean implements Serializable {
+//	private String path = "http://130.237.84.211:8080/Faceoogle2/rest/";
+//	private static final long serialVersionUID = 1L;
+//	private List<ChatViewModel> chatMessages = new ArrayList<ChatViewModel>();
+//	private String paramUser = ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+//			.getRequest()).getParameter("user");
+//	private String message;
+//	@ManagedProperty(value = "#{userBean}")
+//	private UserBean userBean;
+//
+//	public void setUserBean(UserBean userBean) {
+//		this.userBean = userBean;
+//	}
+//	
+//	@SuppressWarnings("unchecked")
+//	public List<ChatViewModel> getChatMessages() {
+//		RestClient client = new RestClient();
+//		Resource res = client.resource(path + "chat/history?chater=" + userBean.getUsername() + "&chatee="+paramUser);
+//		String jsonChat = res.accept("application/json").get(String.class);
+//		Gson gson = new Gson();
+//		chatMessages = gson.fromJson(jsonChat, ArrayList.class);
+//		return chatMessages;
+//	}
+//
+//	public void setChatMessages(List<ChatViewModel> chatMessages) {
+//		this.chatMessages = chatMessages;
+//	}
+//
+//	public String getParamUser() {
+//		return paramUser;
+//	}
+//
+//	public void setParamUser(String paramUser) {
+//		this.paramUser = paramUser;
+//	}
+//	
+//	public String sendMessage() {
+//		if(paramUser == null) {
+//			return "index.xhtml";
+//		}
+//		Map<String, String> msg = new HashMap<String, String>();
+//		msg.put("chater", userBean.getUsername());
+//		msg.put("chatee", paramUser);
+//		msg.put("message", message);
+//		Gson gson = new Gson();
+//		String json = gson.toJson(msg);
+//		RestClient client = new RestClient();
+//		Resource resource = client.resource(path + "chat/sendmessage");
+//		resource.contentType("application/json").accept("text/plain").post(String.class, json);
+//		return null;
+//	}
+//
+//	public String getMessage() {
+//		return message;
+//	}
+//
+//	public void setMessage(String message) {
+//		this.message = message;
+//	}
+//}
--- JustChatApp/src/com/justchatapp/bean/UserBean.java
+++ JustChatApp/src/com/justchatapp/bean/UserBean.java
@@ -1,13 +1,10 @@
 package com.justchatapp.bean;
 
 import java.io.Serializable;
-import java.sql.Date;
 import java.util.HashMap;
-import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 
 import org.apache.wink.client.Resource;
 import org.apache.wink.client.RestClient;
@@ -17,138 +14,41 @@ import com.google.gson.Gson;
 @SessionScoped
 @ManagedBean(name = "userBean")
 public class UserBean implements Serializable {
-	private String path = "http://130.237.84.211:8080/Faceoogle2/rest/";
+	private String path = "http://130.237.84.211:8080/justchat/rest/";
 	private static final long serialVersionUID = 1L;
-	private String username;
-	private String password;
-	private String name;
-	private Date birthdate;
-	private String gender;
-	private String loginMessage, registerMessage;
-	private boolean loggedIn;
+	private String fullname;
+	private String gmail;
 
-	public java.util.Date getBirthdate() {
-		return birthdate;
+	public String getFullname() {
+		return fullname;
 	}
 
-	public void setBirthdate(java.util.Date birthdate) {
-		this.birthdate = new java.sql.Date(birthdate.getTime());
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
 	}
 
-	public String getGender() {
-		return gender;
+	public String getGmail() {
+		return gmail;
 	}
 
-	public void setGender(String gender) {
-		this.gender = gender;
+	public void setGmail(String gmail) {
+		this.gmail = gmail;
 	}
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getLoginMessage() {
-		return loginMessage;
-	}
-
-	public void setLoginMessage(String loginMessage) {
-		this.loginMessage = loginMessage;
-	}
-
-	public String getRegisterMessage() {
-		return registerMessage;
-	}
-
-	public void setRegisterMessage(String registerMessage) {
-		this.registerMessage = registerMessage;
-	}
-	
 	public String login() {
-		Map<String, String> user = new HashMap<String, String>();
-		user.put("username", username);
-		user.put("password", password);
+		HashMap<String, String> user = new HashMap<String, String>();
+		user.put("username", gmail);
+		user.put("phonenumber", fullname);
 		Gson gson = new Gson();
 		String json = gson.toJson(user);
 		RestClient client = new RestClient();
 		Resource resource = client.resource(path + "user/login");
 		String response = resource.contentType("application/json").accept("text/plain").post(String.class, json);
 		if (response.equals("200")) {
-			loginMessage = "";
-			loggedIn = true;
 			return "index.xhtml";
 		} else {
-			loginMessage = "Wrong username or password";
-			loggedIn = false;
-			return "login.xhtml";
+			return "login.jsf";
 		}
 	}
 
-	public String register() {
-		Map<String, String> user = new HashMap<String, String>();
-		user.put("username", username);
-		user.put("password", password);
-		user.put("name", name);
-		user.put("birthdate", String.valueOf(birthdate));
-		user.put("gender", gender);
-		Gson gson = new Gson();
-		String json = gson.toJson(user);
-		RestClient client = new RestClient();
-		Resource resource = client.resource(path + "user/register");
-		String response = resource.contentType("application/json").accept("text/plain").post(String.class, json);
-		System.out.println("AAWDO " + response);
-		
-		if (response.equals("200")) {
-			registerMessage = "";
-			return "index.xhtml";
-		} else {
-			registerMessage = "Please fill in all the fields in the correct format";
-			return "register.xhtml";
-		}
-	}
-
-	public String nodejs() {
-		return "nodejs.xhtml";
-	}
-	
-	public String vertx() {
-		return "vertx.xhtml";
-	}
-
-	public String logout() {
-		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-		loggedIn = false;
-		return "login.xhtml";
-	}
-
-	public String gotoMyProfile() {
-		return "profile.xhtml?faces-redirect=true" + "&user=" + username;
-	}
-
-	public boolean getLoggedIn() {
-		return loggedIn;
-	}
-
-	public void setLoggedIn(boolean loggedIn) {
-		this.loggedIn = loggedIn;
-	}
 }
